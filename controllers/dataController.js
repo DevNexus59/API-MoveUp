062836d2c01707bce0ea6dc19a40d42c1e3ff054
Pierre FOURDIN <fourdin.pfmg@gmail.com>
2025-11-25 11:47:40 +0100

    add mvc arch

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -4,7 +4,7 @@ import { readExercises } from "../models/exercisesModel.js";
 export const getCoach = async (req,res) => {
     try {
         const coachs = await readCoach();
-        res.json({ coachs });
+        res.json( coachs );
     } catch (error) {
         console.error("Erreur lecture coach:", error);
         res.status(500).json({ message: "Erreur serveur." });
