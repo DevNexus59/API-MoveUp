9034e9820896165dc0c398d255b504f6983063c4
Joachim MASSON <joachim.masson.17@gmail.com>
2025-11-26 16:08:47 +0100

    add route for one exercice

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getCoach,getExercises} from "../controllers/dataController.js";
+import {
+	getCoach,
+	getExercises,
+	getExerciseId,
+} from "../controllers/dataController.js";
 const router = express.Router();
 
 router.get("/coach", getCoach);
 router.get("/exercices", getExercises);
+router.get("/exercices/:id", getExerciseId);
 
 export default router;
